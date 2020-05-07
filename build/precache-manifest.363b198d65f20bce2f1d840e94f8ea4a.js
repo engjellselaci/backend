10a0f8ab5815e02416639760f607959830e62b77
engjellselaci <engjellerza@hotmail.com>
2020-05-07 12:21:36 +0300

    build

---
+++ build/precache-manifest.363b198d65f20bce2f1d840e94f8ea4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb973ec6c5efbf4691140b676449846b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed654687d46ccc10212",
+    "url": "/static/css/main.54695ac2.chunk.css"
+  },
+  {
+    "revision": "86b8117ff6d59328dad4",
+    "url": "/static/js/2.c4db38da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4db38da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed654687d46ccc10212",
+    "url": "/static/js/main.1051879c.chunk.js"
+  },
+  {
+    "revision": "e65045db3937f27c7418",
+    "url": "/static/js/runtime-main.507620f2.js"
+  }
+]);
